c1e9bd49feaeaf649b7d4f1042caa0de1c4b0084
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-25 14:08:26 +0530

    new file:   app/app.js 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from 'http';
+import app from './app/app.js';
+
+const PORT = process.env.PORT || 7000;
+const server = http.createServer(app);
+
+server.listen(PORT,(err)=>
+{
+    if(err)
+    {
+        return err;
+    }
+
+    console.log(`Server is up and running on port ${PORT}`);
+});
