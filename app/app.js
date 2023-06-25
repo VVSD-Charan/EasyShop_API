27c22945ac2a33a804fc6604278c6823eeb57e81
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-25 14:37:20 +0530

    modified:   .gitignore 	modified:   app/app.js 	new file:   config/dbConnect.js 	modified:   package-lock.json 	modified:   package.json

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
 import express from 'express';
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+import dbConnect from '../config/dbConnect.js';
+
+
+dbConnect();
 const app =express();
 
 export default app;
