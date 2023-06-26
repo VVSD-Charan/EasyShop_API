7e7c85c9c1f5779deb608d30d15c69f668b7da61
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-26 18:41:24 +0530

    modified:   controllers/categoriesCtrl.js 	modified:   routes/categoriesRouter.js

--- controllers/categoriesCtrl.js
+++ controllers/categoriesCtrl.js
@@ -1,7 +1,7 @@
 import asyncHandler from 'express-async-handler';
 import Category from "../model/Category.js";
 
-// category endpoint /api/v1/categories
+// category endpoint POST /api/v1/categories
 
 export const createCategoryCtrl = asyncHandler(
     async (req , res) =>
@@ -30,3 +30,60 @@ export const createCategoryCtrl = asyncHandler(
         })
     }
 ) ;
+
+// Get all categories API end point : GET /api/v1/categories
+export const getAllCategoriesCtrl = asyncHandler (
+    async (req , res) =>
+    {
+        const categories =await Category.find();
+
+        res.json({
+            status : "success",
+            message : " All categories have been fetched",
+            categories
+        })
+    }
+);
+
+// Get single category API end point: GET /api/v1/categories/:id
+export const getSingleCategoryCtrl = asyncHandler(
+    async ( req , res) =>
+    {
+        const category = await Category.findById(req.params.id);
+
+        res.json({
+            status : "success",
+            message : "Category has been fetched",
+            category,
+        })
+    }
+);
+
+//Update category endpoint PUT /api/v1/categories/:id
+export const updateCategoryCtrl = asyncHandler (
+    async ( req , res) =>
+    {
+        const {name} = req.body;
+
+        const category = await Category.findByIdAndUpdate(req.params.id,{name},{new:true});
+
+        res.json({
+            status : "success",
+            message : "Category has been updated",
+            category
+        })
+    }
+);
+
+// Delete category API end point DELETE /api/v1/categories/:id
+export const deleteCategoryCtrl = asyncHandler(
+    async (req , res) =>
+    {
+        const category = await Category.findByIdAndDelete(req.params.id);
+
+        res.json({
+            status : "success",
+            message : "Category has been deleted successfully",
+        })
+    }
+);
