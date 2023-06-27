22df1ab2725dfa3020f6266531849ea56687649c
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-27 21:04:16 +0530

    modified:   app/app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import Stripe from 'stripe';
 dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
@@ -14,6 +15,42 @@ import orderRouter from '../routes/ordersRouter.js';
 dbConnect();
 const app =express();
 
+//Stripe web hook
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_7f5c646f172ae871cfd0d34d829c0ad4590d5847dda0e3fdce1bf05acfee9e70";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    console.log("event");
+  } catch (err) {
+    console.log("err",err.message);
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
+
 //Pass incoming data
 app.use(express.json());
 
@@ -24,6 +61,7 @@ app.use("/api/v1/brands/",brandsRouter);
 app.use("/api/v1/colors/",colorsRouter);
 app.use("/api/v1/reviews/",reviewRouter);
 app.use("/api/v1/orders/",orderRouter);
+
 
 app.use(notFound);
 app.use(globalErrHandler);
